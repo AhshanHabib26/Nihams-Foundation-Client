f9d4aa372c689ca05c046766fcd795aabec5c115
AhshanHabib26 <habib.net27@gmail.com>
2025-03-18 02:34:45 +0600

    TopNav Added

--- src/lib/Container.tsx
+++ src/lib/Container.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 const Container = ({ children }: { children: ReactNode }) => {
-  return <div className="w-full max-w-[1180px] px-3 mx-auto">{children}</div>;
+  return <div className="w-full max-w-[1180px] px-5 mx-auto">{children}</div>;
 };
 
 export default Container;
--- src/shared/Navbar/TopNav.tsx
+++ src/shared/Navbar/TopNav.tsx
@@ -1,7 +1,30 @@
+import { Button } from "@/components/ui/button"
+import Container from "@/lib/Container"
+import { FaEnvelope, FaFacebook, FaPhone, FaSun, FaYoutube } from "react-icons/fa6"
 
 const TopNav = () => {
   return (
-    <div>TopNav</div>
+    <div className="bg-gray-800 text-white">
+      <Container>
+        <div className="flex items-center justify-between py-3">
+          <div className="flex space-x-3 cursor-pointer">
+            <FaFacebook size={18} className=" hover:text-blue-500" />
+            <FaYoutube size={18} className=" hover:text-red-500" />
+            <FaEnvelope size={18} className=" hover:text-orange-500" />
+            <FaPhone size={18} className=" hover:text-green-500" />
+          </div>
+          <div className="flex items-center space-x-3">
+            <div>
+              <FaSun color="orange" />
+            </div>
+            <div className="flex items-center">
+              <Button size="sm" className="bg-green-500 rounded-none cursor-pointer">EN</Button>
+              <Button size="sm" className=" bg-white text-black rounded-none cursor-pointer">বাংলা</Button>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
   )
 }
 
